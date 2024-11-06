7ccc72056ddc5bd6f9db5c8e629917060936e632
CT101/G/22504/24 Immanuel Bett <kipronoimmanuel84@gmail.com>
2024-11-06 21:42:10 +0300

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,26 @@
+/*Aurthor :Immanuel Bett
+Reg No :CT101/G/22504/24
+*/
+#include <stdio.h>
+#include <string.h>
+struct employee{
+	char name[25];
+	int id;
+	char department[20];
+	float salary;
+	char email[50];
+}employee;
+
+int main(){
+strcpy(employee.name,"John_Doe");
+employee.id = 12345;
+strcpy(employee.department,"Human _Resource");
+employee.salary = 55000.50;
+strcpy(employee.email,"john.doe@company.com");
+
+printf("Name:%s\n",employee.name);
+printf("ID:%d\n",employee.id);
+printf("Department:%s\n",employee.department);
+printf("email:%s\n",employee.email);
+return 0;
+}
